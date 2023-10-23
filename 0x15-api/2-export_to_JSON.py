2dd3093ebac46c8f32e8047a1405f5892b69ae9b
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-10-23 18:13:02 +0100

    Task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
 
 """
-A Python script that, using this REST API, for a given employee ID, returns
-information about his/her TODO list progress.
+This Python script fetches data from a REST API for a given employee ID and
+exports their task information in JSON format.
 """
 
 import json
 import requests
-
-
-def export_todo_list_to_json():
-    base_url = "https://jsonplaceholder.typicode.com"
-    users = requests.get(f"{base_url}/users").json()
-
-    todo_data = {}
-    for user in users:
-        user_id = user["id"]
-        user_name = user["username"]
-        todos = requests.get(f"{base_url}/todos", params={
-                                "userId": user_id}).json()
-
-        user_todos = [{"task": todo["title"], "completed": todo["completed"],
-                       "username": user_name} for todo in todos]
-        todo_data[user_id] = user_todos
-
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump(todo_data, jsonfile, indent=4)
-
+import sys
 
 if __name__ == "__main__":
-    export_todo_list_to_json()
+    user_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user_data = requests.get(base_url + "users/{}".format(user_id)).json()
+    username = user_data.get("username")
+    tasks_list = requests.get(base_url + "todos", params={
+                                        "userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as json_file:
+        json.dump(
+            {
+                user_id: [
+                    {
+                        "task": task.get("title"),
+                        "completed": task.get("completed"),
+                        "username": username,
+                    }
+                    for task in tasks_list
+                ]
+            },
+            json_file,
+        )
