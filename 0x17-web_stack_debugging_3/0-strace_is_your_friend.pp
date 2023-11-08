170ddf32763ac7aa68ea07206264ef72e93c607c
Abdessamad HADDOUCHE <abdessamad.hadd.com>
2023-11-08 14:33:53 -0800

    Web stack debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+#replace "phpp" with "php"
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
+}
